30ddd908e0e9e1cf3c73a7fa28ece3882bbe98fc
박승용 <intotheakira@gmail.com>
2019-10-26 18:21:46 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {Header} from "./component/header";
 import {Player} from "./component/player";
+import {AddPlayerForm} from "./component/AddPlayerForm";
 
 class App extends React.Component {
   state = {
@@ -56,6 +57,7 @@ class App extends React.Component {
                                                  changeScore={this.handleChangeScore.bind(this)}
                                                  id={item.id} />)
         }
+        <AddPlayerForm/>
       </div>
     );
   }
